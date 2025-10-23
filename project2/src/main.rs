6ec46c24d5a6155d0faa6a053256c05c6913fd76
ModelSM9208 <153126371+ModelSM9208@users.noreply.github.com>
2025-10-22 22:28:12 -0500

    fantastic (first) four (#1)
    * created beta function in project2 that creates three variables x,y,z
    x is unused y and z are compared
    if y and z are equal print to screen
    
    * refactored beta function name for alpha, created empty beta function
    
    * Changes to be committed:
    	modified:   project2/src/main.rs
    
    * Created two functions gamma and delta
    gamma creates a number and prints out it's value
    delta mutates that number before returning to the flow of gamma
    
    ---------
    
    Co-authored-by: William Smith <william.smith@modelsm9208.com>

--- project2/src/main.rs
+++ project2/src/main.rs
@@ -1,3 +1,30 @@
+fn alpha(){
+    let _x: &'static str = "two";
+    let y: i32 = 2;
+    let z: i32 = 2;
+    assert_eq!(y,z);
+    println!("y:{} equals z:{}",y,z)
+}
+
+fn beta(){
+    let x: &'static str = "One";
+    let y: &'static str = "Two";
+    let z: &'static str = "Three";
+    print!("{} something's got to give \n{} something's got to give \n{} something's got to give\n",x,y,z)
+}
+
+fn gamma(){
+    let mut number: i32 = 1;
+    println!("before delta {}",number);
+    delta(&mut number);
+    println!("after delta {}",number)
+}
+fn delta(number: &mut i32){
+    *number += 2;
+}
+
 fn main() {
-    println!("Hello, world!");
+    alpha();
+    beta();
+    gamma();
 }
