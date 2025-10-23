edac3b2077daf088c3add298f0f0d87d38cee498
William Smith <william.smith@modelsm9208.com>
2025-10-22 21:48:46 -0500

    created beta function in project2 that creates three variables x,y,z x is unused y and z are compared if y and z are equal print to screen

--- project2/src/main.rs
+++ project2/src/main.rs
@@ -1,3 +1,11 @@
+fn beta(){
+    let _x: &'static str = "two";
+    let y: i32 = 2;
+    let z: i32 = 2;
+    assert_eq!(y,z);
+    println!("y:{} equals z:{}",y,z)
+}
+
 fn main() {
-    println!("Hello, world!");
+    beta();
 }
