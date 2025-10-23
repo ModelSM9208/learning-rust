3734c152c44decfdd7f2c3b1400abc6236238138
William Smith <william.smith@modelsm9208.com>
2025-10-22 22:21:22 -0500

    Created two functions gamma and delta gamma creates a number and prints out it's value delta mutates that number before returning to the flow of gamma

--- project2/src/main.rs
+++ project2/src/main.rs
@@ -13,7 +13,18 @@ fn beta(){
     print!("{} something's got to give \n{} something's got to give \n{} something's got to give\n",x,y,z)
 }
 
+fn gamma(){
+    let mut number: i32 = 1;
+    println!("before delta {}",number);
+    delta(&mut number);
+    println!("after delta {}",number)
+}
+fn delta(number: &mut i32){
+    *number += 2;
+}
+
 fn main() {
     alpha();
     beta();
+    gamma();
 }
