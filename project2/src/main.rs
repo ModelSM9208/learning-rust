05252a1c917facdfb74615deace934223824cb65
Model_SM9208 <lab@modelsm9208.com>
2026-05-20 20:14:41 +0000

    cargo fmt

--- project2/src/main.rs
+++ project2/src/main.rs
@@ -1,25 +1,28 @@
-fn alpha(){
+fn alpha() {
     let _x: &'static str = "two";
     let y: i32 = 2;
     let z: i32 = 2;
-    assert_eq!(y,z);
-    println!("y:{} equals z:{}",y,z)
+    assert_eq!(y, z);
+    println!("y:{} equals z:{}", y, z)
 }
 
-fn beta(){
+fn beta() {
     let x: &'static str = "One";
     let y: &'static str = "Two";
     let z: &'static str = "Three";
-    print!("{} something's got to give \n{} something's got to give \n{} something's got to give\n",x,y,z)
+    print!(
+        "{} something's got to give \n{} something's got to give \n{} something's got to give\n",
+        x, y, z
+    )
 }
 
-fn gamma(){
+fn gamma() {
     let mut number: i32 = 1;
-    println!("before delta {}",number);
+    println!("before delta {}", number);
     delta(&mut number);
-    println!("after delta {}",number)
+    println!("after delta {}", number)
 }
-fn delta(number: &mut i32){
+fn delta(number: &mut i32) {
     *number += 2;
 }
 
