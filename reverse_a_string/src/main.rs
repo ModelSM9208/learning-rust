d9dd2747b5fcfc4cdbf2821f472fbdddbd3e681f
Model_SM9208 <lab@modelsm9208.com>
2026-07-02 23:01:51 +0000

    Changes to be committed: 	new file:   reverse_a_string/Cargo.lock 	modified:   reverse_a_string/src/main.rs

--- reverse_a_string/src/main.rs
+++ reverse_a_string/src/main.rs
@@ -1,3 +1,13 @@
+use std::io::{self, Write};
+
 fn main() {
-    println!("Hello, world!");
+    print!("Enter a string to reverse: ");
+    io::stdout().flush().unwrap();
+
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+
+    let reversed: String = input.trim().chars().rev().collect();
+
+    println!("Reversed: {reversed}");
 }
